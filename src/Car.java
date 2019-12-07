826b0615b33c6f00c6a1268da0b5734bc931f741
„Tdecazgit config --global user.email „tadas.kaluina@gmail.com <tadas.kaluina@gmail.com>
2019-12-07 11:08:29 +0200

    uncommnet

--- src/Car.java
+++ src/Car.java
@@ -1,19 +1,16 @@
 public class Car {
 
-//    private String name;
+    private String name;
     private String model;
-   // private double price;
-   // private int quantity;
-
+    private double price;
+    private int quantity;
 
     Car(String name) {
+        this.price = price;
+        this.name = name;
+        this.quantity = quantity;
+    }
 
-     /*   {
-            this.price = price;
-            this.name = name;*/
-            this.quantity = quantity;
-    }
-    }
     public String getName() {
         return name;
     }
@@ -21,6 +18,7 @@ public class Car {
     public double getPrice() {
         return price;
     }
+
     public int getQuantity() {
         return quantity;
     }
